7468bbd1d4e2419295b661d3474baffc320c72c5
Stumpp-KUN <kiril.bychkov.03@mail.ru>
2023-02-14 18:18:03 +0300

    add

---
+++ school-schedule/src/test/java/com/solvd/schedule/schoolschedule/SchoolScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solvd.schedule.schoolschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
